223ff693e6453d33e2e1a8919d29548242ae147f
averdon <anthonyverdon10@gmail.com>
2025-03-28 12:47:50 +0100

    move set_targetdir into target to avoid problem when including in others modules

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,3 @@
-set_targetdir("./")
 set_languages("cxx17")
 
 set_toolchains("clang")
@@ -8,10 +7,12 @@ set_symbols("debug")
 add_requires("doctest")
 
 target("Matrix")
+    set_targetdir("./")
     set_kind("headeronly")
     add_includedirs("srcs", {public = true})
 
 target("Tester")
+    set_targetdir("./")
     set_kind("binary")
     add_files("tests/**.cpp")
     add_deps("Matrix")
